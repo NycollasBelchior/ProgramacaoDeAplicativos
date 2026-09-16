a1c28dc0d6f8fee616e165dfc1055e39c7ff0414
NYCOLLAS CORDEIRO NORONHA BELCHIOR <nycollas_belchior@estudante.sesisenai.org.br>
2026-09-15 21:44:09 -0300

    primeiro site

--- introducao/back.js
+++ introducao/back.js
@@ -1,5 +1,19 @@
 function clicou(){
     let nome = document.getElementById("nome").value;
-    alert(nome)
+    let password = document.getElementById("password").value
+    let data = document.getElementById("data").value
+    
+    
+    let select = document.getElementById("seletorLanguage")
+      
+    let escolhido = select.value
+    
+    
+    
+   
 
+
+    
+    alert("nome: "+nome +"\n senha: " + password+ "\n data: " + data + "\nlinguagem escolhida: " + escolhido)
 }
+
