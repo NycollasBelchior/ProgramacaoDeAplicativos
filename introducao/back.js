3bcadbbcaddd513467acd0f461e77ad27c5b7fdd
NYCOLLAS CORDEIRO NORONHA BELCHIOR <nycollas_belchior@estudante.sesisenai.org.br>
2026-09-21 21:45:26 -0300

    copia insta

--- introducao/back.js
+++ introducao/back.js
@@ -1,19 +1,31 @@
-function clicou(){
+let botao = document.getElementById("botaoCadastro");
+
+botao.addEventListener("click", function () {
+
     let nome = document.getElementById("nome").value;
-    let password = document.getElementById("password").value
-    let data = document.getElementById("data").value
-    
-    
-    let select = document.getElementById("seletorLanguage")
-      
-    let escolhido = select.value
-    
-    
-    
-   
+    let password = document.getElementById("password").value;
+    let gmail = document.getElementById("gmail").value;
+    let select = document.getElementById("seletorLanguage");
+    let escolhido = select.value;
 
+    let cadastrado = false
+    
+    if (
+        nome.trim() !== "" &&
+        password.trim() !== "" &&
+        gmail.trim() !== ""
+    ) {
+        alert(
+            "nome: " + nome +
+            "\nsenha: " + password +
+            "\ndata: " + gmail +
+            "\nlinguagem escolhida: " + escolhido
+        );
+        cadastrado = true
+    } else {
+        alert("Preencha todos os campos!");
+    }
 
     
-    alert("nome: "+nome +"\n senha: " + password+ "\n data: " + data + "\nlinguagem escolhida: " + escolhido)
-}
+});
 
